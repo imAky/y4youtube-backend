1788d05ecb9e7872d66eee92ae2da42d1b8e92f1
imAky <aky@example.com>
2024-07-08 19:24:06 +0530

    y4youtube backend

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import videoRoutes from "./routes/videoRoutes.js";
+import cors from "cors";
+import job from "./cron/cron.js";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+job.start();
+
+const app = express();
+
+// Middleware
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "https://y4youtube.vercel.app",
+    optionsSuccessStatus: 200,
+  })
+);
+
+// Routes
+
+app.use("/videos", videoRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server connected on PORT ${PORT}`);
+});
